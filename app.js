8c6bf4f854c71fb60259893f166b967501663403
shyu <qaz159qaz159@gmail.com>
2021-09-14 15:20:22 +0800

    update some basic settings

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require("path");
 const app = express()
 
 app.use('/public', express.static(path.join(__dirname, './public')))
+app.use('/node_modules', express.static(path.join(__dirname, './node_modules')))
 
 app.engine('html', require('express-art-template'))
 app.set('views', {
--- router/index.js
+++ router/index.js
@@ -6,6 +6,16 @@ router.get('/', (req, res) => {
     res.render('index.html')
 })
 
+router.get('/login', (req, res) => {
+    res.render('login.html', {
+        isLogin: true
+    })
+})
+
+router.get('/register', (req, res) => {
+    res.render('login.html')
+})
+
 // // user related router
 // router.use(require('./user'))
 //
